e3adc5ac20b155237748f642e93b327dc3575c5a
ayalaillouz <a0534195130@gmail.com>
2024-05-28 22:29:02 +0300

    Add project files.

---
+++ Exeption.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 5;
+	int* b;
+	try
+	{
+		b = &a;
+		cout << b << endl;
+	}
+	catch (char* ex)
+	{
+	
+		throw "throwing an exception";
+	}
+	int arr[3] = { 2,5,4 };
+	try
+	{
+		a=arr[20];
+	}
+
+	catch (char* ex)
+	{
+
+		throw "throwing an exception";
+	}
+	try
+	{
+		cout << *(b+2) << endl;
+	
+	}
+
+	catch (char* ex)
+	{
+
+		throw "throwing an exception";
+	}
+	catch (...) 
+	{
+		cout << "Good😊😊" << endl;
+	}
+}
